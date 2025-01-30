627ce4e0a1b9c3789328c2db139b1bea7cc8434f
Isaiah Swain <isaiah.m.swain@gmail.com>
2025-01-30 10:37:55 -0500

    Button now plays a note

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Image from "next/image";
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
@@ -5,16 +7,23 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { PlayCircleFill } from 'react-bootstrap-icons';
 
-function tonePlayer() {
-  const audioCtx = new AudioContext();
-  const oscillator = audioCtx.createOscillator();
-  oscillator.type = "square";
-  oscillator.frequency.setValueAtTime(440, audioCtx.currentTime);
-  oscillator.connect(audioCtx.destination);
-  oscillator.start();
-}
-
 export default function Home() {
+  const audioCtx = new AudioContext();
+  let playing = false;
+
+  function handlePlay() {
+    if (playing)
+      return;
+    const oscillator = audioCtx.createOscillator();
+    oscillator.type = "square";
+    oscillator.frequency.setValueAtTime(440, audioCtx.currentTime);
+    oscillator.connect(audioCtx.destination);
+    oscillator.start();
+    playing = true;
+    setTimeout(() => oscillator.stop(), 1000);
+    playing = false;
+  }
+
   return (
     // <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start justify-center">
       <>
@@ -25,7 +34,7 @@ export default function Home() {
         <h1 className="text-4xl sm:text-5xl lg:text-6xl leading-none font-extrabold text-grey tracking-tight mb-8">Toney</h1>
         <p className="text-lg sm:text-xl lg:text-2xl font-medium mb-6 text-grey">Ear trainer for the tone deaf</p>
         <p>Instructions go here</p>
-        <Button variant="primary" size="lg" onclick={tonePlayer()}><PlayCircleFill color="black" style={{display: "inline", margin: "5px"}}/>Start</Button>
+        <Button variant="primary" size="lg" onClick={handlePlay}><PlayCircleFill color="black" style={{display: "inline", margin: "5px"}}/>Start</Button>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <Button variant="primary" size="lg">Higher</Button>
           <Button variant="primary" size="lg">Same</Button>
